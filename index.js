7373f0b0969b64280fd8c7eabfd2613365e93cf5
rajparmar03 <rjbusiness03@gmail.com>
2023-05-26 00:57:59 +0530

    created connection and server for the backend application.

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+
+const { connection } = require("mongoose");
+
+const port = process.env.PORT;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port , async() => {
+    try {
+        await connection;
+        console.log("successfully connected to DB...");
+    } catch (error) {
+        console.log("failed to connect to DB...");
+    }
+    console.log("server is successfully running...");
+})
